a9cfc69753a876bcf7fb083b27975c181c118130
Luis Martinez <s_k_ap3@hotmail.com>
2021-11-12 03:28:14 -0600

    definiendo el modelo del post y categorias

--- content_blog/models.py
+++ content_blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class post(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4)
+    contenido = models.TextField()
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    img = models.CharField(max_length=100)
+
+
+class categorias_post(models.Model):
+    id_categorias = models.ForeignKey('catalogo_categorias',on_delete=models.CASCADE)
+    id_post = models.ForeignKey('post',on_delete=models.CASCADE)
+
+
+class catalogo_categorias(models.Model):
+    nombre = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=60)
+    status = models.BooleanField(default=True)
